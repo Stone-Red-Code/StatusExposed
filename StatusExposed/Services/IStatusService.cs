a4675ce85c5a11ace4a4f820004394f4a22f6f85
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2022-06-24 19:21:19 +0200

    Add comments to `StatusService` and `StatusPage`

--- StatusExposed/Services/IStatusService.cs
+++ StatusExposed/Services/IStatusService.cs
@@ -4,9 +4,26 @@ namespace StatusExposed.Services;
 
 public interface IStatusService
 {
+    /// <summary>
+    /// Gets the current status of a service.
+    /// </summary>
+    /// <param name="domain">Domain of the service.</param>
+    /// <returns><see cref="StatusInformation"/> for the requested service.</returns>
     Task<StatusInformation?> GetStatusAsync(string domain);
 
+    /// <summary>
+    /// Adds a new service.
+    /// </summary>
+    /// <param name="domain">Domain of the service.</param>
+    /// <param name="statusPageUrl">Official status page of the service.</param>
+    /// <returns>A <see cref="Task"/> to await</returns>
     Task AddServiceAsync(string domain, string? statusPageUrl);
 
+    /// <summary>
+    /// Gets the statuses of all services in a specified range.
+    /// </summary>
+    /// <param name="index">Start index of services.</param>
+    /// <param name="count">The max amount of services to return.</param>
+    /// <returns>The <see cref="StatusInformation"/>s of all services.</returns>
     IEnumerable<StatusInformation> GetStatuses(int index, int count);
 }
