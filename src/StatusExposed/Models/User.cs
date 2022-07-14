9357f3871db2b1494c2a143218dde1edd1410cee
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2022-07-14 17:10:44 +0200

    Add basics for account system

---
+++ src/StatusExposed/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StatusExposed.Models;
+
+public class User
+{
+    public User(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public List<string> Permissions { get; init; } = new List<string>();
+
+    public bool HasPermission(string permission)
+    {
+        return Permissions.Contains(permission);
+    }
+}
---
+++ src/StatusExposed/Utilities/SecureStringGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+
+namespace StatusExposed.Utilities;
+
+public static class SecureStringGenerator
+{
+    public static string CreateCryptographicRandomString(int count)
+    {
+        return Convert.ToBase64String(RandomNumberGenerator.GetBytes(count));
+    }
+}
